917922744d4fa89f711f7429fe4bc4bb407b3ae1
SafronovMaks1m <safranov01@list.ru>
2026-02-28 20:06:59 +0300

    fix

--- out/build/x64-Debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/ST-1/src
-# Build directory: F:/ST-1/out/build/x64-Debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/x64-Debug/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/ST-1/test
-# Build directory: F:/ST-1/out/build/x64-Debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/googletest-build")
