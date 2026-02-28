c888be9822f6b2420a923400817698c399d90e3a
SafronovMaks1m <safranov01@list.ru>
2026-02-28 20:28:22 +0300

    fix ccplint

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,57 +2,58 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-    if (value < 2) {
-        return false;
-    }
+  if (value < 2) {
+    return false;
+  }
 
-    for (uint64_t i = 2; i * i <= value; i++) {
-        if (value % i == 0)
-            return false;
+  for (uint64_t i = 2; i * i <= value; i++) {
+    if (value % i == 0) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) {
-        return 0;
+  if (n == 0) {
+    return 0;
+  }
+
+  uint64_t number = 0;
+  uint64_t value = 2;
+
+  while (number < n) {
+    if (checkPrime(value)) {
+      number += 1;
     }
-
-    uint64_t number = 0;
-    uint64_t value = 2;
-
-    while (number < n) {
-        if (checkPrime(value)) {
-            number += 1;
-        }
-        if (number < n)
-            value += 1;
+    if (number < n) {
+      value += 1;
     }
+  }
 
-    return value;
+  return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t cur = value + 1;
+  uint64_t cur = value + 1;
 
-    while (checkPrime(cur) != true) {
-        cur += 1;
-    }
+  while (checkPrime(cur) != true) {
+    cur += 1;
+  }
 
-    return cur;
+  return cur;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-   uint64_t sum = 0;
+  uint64_t sum = 0;
 
-   for (uint64_t i = 2; i < hbound; i++) {
-       if (checkPrime(i)) {
-           sum += i;
-       }
-   }
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
 
-   return sum;
+  return sum;
 }
